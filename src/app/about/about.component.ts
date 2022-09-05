9b8ca055d41a5c6f8fb07079c7fd818a5b40358c
Noemie Brancourt <noemie.brancourt@itonics.de>
2022-09-05 12:52:19 +0200

    converting json response into array

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { noop } from "rxjs";
+import { map } from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
-  }
+    const http$ = createHttpObservable("/api/courses");
 
+    const courses$ = http$.pipe(map((res) => Object.values(res["payload"])));
+
+    courses$.subscribe(
+      (courses) => console.log(courses),
+      noop,
+      () => console.log("completed")
+    );
+  }
 }
