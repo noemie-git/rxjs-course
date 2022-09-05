6976270f874bb17bed1ab816d1858ab13959a2ca
Noemie Brancourt <noemie.brancourt@itonics.de>
2022-09-05 14:56:17 +0200

    returning filtered courses reactively

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,25 +17,25 @@ import { createHttpObservable } from "../common/util";
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  beginnerCourses: Course[];
-  advancedCourses: Course[];
+  beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
 
   ngOnInit() {
     const http$ = createHttpObservable("/api/courses");
 
-    const courses$ = http$.pipe(map((res) => Object.values(res["payload"])));
+    const courses$: Observable<Course[]> = http$.pipe(
+      map((res) => Object.values(res["payload"]))
+    );
 
-    courses$.subscribe(
-      (courses) => {
-        this.beginnerCourses = courses.filter(
-          (course) => course.category == "BEGINNER"
-        );
-        this.advancedCourses = courses.filter(
-          (course) => course.category == "ADVANCED"
-        );
-      },
-      noop,
-      () => console.log("completed")
+    this.beginnerCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category == "BEGINNER")
+      )
+    );
+    this.advancedCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category == "ADVANCED")
+      )
     );
   }
 }
